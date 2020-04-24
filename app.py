69c55ca19096ea24fc64ea9f791dce12af96e7e9
filipsl <filip.slazyk@gmail.com>
2020-04-24 13:50:45 +0200

    test travis

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times. This text have been changed\n'.format(count)
+    return 'Hello World! I have been seen {} times. This text have been changed again\n'.format(count)
